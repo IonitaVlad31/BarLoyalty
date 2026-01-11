873153387cbc221ed41f479315df6272dd8857d1
Adrian-Gheorghita Gramada <adrian.gramada@student.unitbv.ro>
2026-01-11 19:18:26 +0200

    Integrate qrencode and PNG support in QR service
    Updated CMakeLists.txt and Dockerfile to add qrencode and libpng dependencies for QR code generation. Modified main.cpp to test QR code library functionality at startup. Adjusted docker-compose.yml port mappings for database and gateway services.

--- qr-service/main.cpp
+++ qr-service/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <qrencode.h>
 
 int main() {
-    std::cout << "QR Service (C++) is starting..." << std::endl;
+    std::cout << "QR Service: Library check..." << std::endl;
 
-    // Simulam un server care ruleaza infinit
+    QRcode *qr = QRcode_encodeString("Test", 0, QR_ECLEVEL_L, QR_MODE_8, 1);
+
+    if (qr != nullptr) {
+        std::cout << "SUCCESS: QR Code library is working!" << std::endl;
+        QRcode_free(qr);
+    } else {
+        std::cout << "ERROR: Could not generate QR." << std::endl;
+    }
+
     while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-        std::cout << "QR Service is alive..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(10));
     }
     return 0;
 }
